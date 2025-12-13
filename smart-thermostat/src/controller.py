ab0fb04da3fb6135cb60e777d9f24e0f8beb9dcb
Kishen S Jain <jain.skishen@gmail.com>
2025-12-13 10:01:04 +0530

    Refactor ThermostatController to implement temperature mode determination

--- smart-thermostat/src/controller.py
+++ smart-thermostat/src/controller.py
@@ -1,13 +1,23 @@
 from src.sensor import TemperatureSensor
-
+from src.modes import HeatMode, CoolMode, OffMode
 class ThermostatController:
-    
     def get_current_temperature(self):
         sensor = TemperatureSensor()
         try:
-            return sensor.read_temperature()
+            curr_temp = sensor.read_temperature()
+            return curr_temp
         except Exception as e:
             return e
     
     def determine_mode(self, temperature):
-        pass
+        if temperature < 20:
+            return HeatMode()
+        elif temperature > 30:
+            return CoolMode()
+        else:
+            return OffMode()
+    
+    def get_mode(self):
+        temperature = self.get_current_temperature()
+        mode = self.determine_mode(temperature)
+        return mode
