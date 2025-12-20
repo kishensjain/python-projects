17ea44c9826170bfe22ad24505e8915f48db9ae3
Kishen S Jain <jain.skishen@gmail.com>
2025-12-20 15:07:09 +0530

    Implement CRUD operations for books

--- 02_crud-fastapi-pydantic/main.py
+++ 02_crud-fastapi-pydantic/main.py
@@ -10,3 +10,33 @@ class Book(BaseModel):
     author: str
 
 books: List[Book] = []
+
+@app.get("/")
+def read_root():
+    return {"message":"Welcome to the library!"}
+
+@app.get("/books")
+def get_books():
+    return books
+
+@app.post("/book")
+def add_book(book: Book):
+    books.append(book)
+    return {"message": "Book added successfully"}, 201,book
+
+@app.put("/book/{id}")
+def update_book(book_id: int, updated_book: Book):
+    for b in books:
+        if b.id == book_id:
+            b.name = updated_book.name
+            b.author = updated_book.author
+            return b, {"message": "Book updated successfully"}
+    return {"message": "Book not found"}, 404
+
+@app.delete("/book/{id}")
+def delete_book(book_id: int):
+    for b in books:
+        if b.id == book_id:
+            books.remove(b)
+            return {"message": "Book deleted successfully"}
+    return {"message": "Book not found"}, 404
